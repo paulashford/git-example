48d3400cbb44ff7309678a0ab7f486801c6c0947
Paul Ashford <paul.ashford@cantab.net>
2025-11-13 14:36:13 +0000

    1 standardise function calls

--- climate_analysis.py
+++ climate_analysis.py
@@ -3,7 +3,7 @@ COMMENT = '#'
 climate_data = open('data/sc_climate_data_10.csv', 'r')
 
 
-def FahrToCelsius(fahr):
+def fahr_to_celsius(fahr):
     """COnverts fahrenehit to celsius
 
     Args:
@@ -14,8 +14,8 @@ def FahrToCelsius(fahr):
     """
     celsius = ((fahr - 32) * (5/9)) 
     return celsius
-def FahrToKelvin(fahr):
-    kelvin = FahrToCelsius(fahr) + 273.15
+def fahr_to_kelvin(fahr):
+    kelvin = fahr_to_celsius(fahr) + 273.15
     return kelvin
 
 
@@ -24,6 +24,6 @@ for line in climate_data:
     data = line.split(',')
     if data[0][0] != COMMENT:
         fahr = float(data[3])
-        celsius = FahrToCels(fahr)
-        kelvin = FahrToKelvin(fahr)
+        celsius = fahr_to_celsius(fahr)
+        kelvin = fahr_to_kelvin(fahr)
         print('Max temperature in Celsius', celsius, 'Kelvin', kelvin)
